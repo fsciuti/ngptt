940349f9e726fc0ad99dfaf7a36b79d506e95aab
Francesco Sciuti <francescosciuti@Francescos-MBP.station>
2019-03-07 18:17:53 +0100

    Update ProjectList Component with injected Project Service

--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -1,63 +1,33 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 import { Project } from '../../shared/Project';
+import { ProjectService } from 'src/app/shared/services/project.service';
 
 @Component({
     selector: 'ngptt-project-list',
     templateUrl: './project-list.component.html'
 })
-export class ProjectListComponent {
-    projects: Project[] = [
-        {
-            id: 1,
-            code: 'NHusYJl',
-            name: 'Progetto Alpha',
-            description: 'Lorem ipsum dolor sit amet.',
-            start: new Date(2019, 1, 31),
-            end: new Date(2019, 3, 15),
-            priority: 'medium',
-            done: true,
-            tasks: []
-        },
-        {
-            id: 2,
-            code: 'SJieYKl',
-            name: 'Progetto Beta',
-            description: 'Lorem ipsum dolor sit amet.',
-            start: new Date(2019, 3, 31),
-            end: new Date(2019, 6, 15),
-            priority: 'low',
-            done: true,
-            tasks: []
-        },
-        {
-            id: 3,
-            code: 'POjeGBs',
-            name: 'Progetto Gamma',
-            description: 'Lorem ipsum dolor sit amet.',
-            start: new Date(2019, 8, 15),
-            priority: 'low',
-            done: false,
-            tasks: []
-        },
-    ];
+export class ProjectListComponent implements OnInit {
+    projects: Project[] = [];
 
     selectedProject: Project;
     searchedProject: Project;
 
-    constructor() { }
+    constructor(private projectService: ProjectService) { }
 
-    selectProject(project: Project) {
-        this.selectedProject = project;
+    ngOnInit() {
+        this.projects = this.projectService.getAll();
     }
 
-    addNewProject(project: Project) {
-        this.projects.push(project);
+    selectProject(project: Project) {
+        this.selectedProject = this.projectService.get(project.id);
+    }
+
+    addProject(project: Project) {
+       this.projectService.add(project);
     }
 
     searchProject(project: Project) {
-        console.log(project);
         this.searchedProject = project;
     }
 }

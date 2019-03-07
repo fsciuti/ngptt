025dbc464211dff77811411c327479077192d1ed
Francesco Sciuti <francescosciuti@Francescos-MBP.station>
2019-03-07 18:00:03 +0100

    Add ProjectList Component CSS File with form fields alert style

--- src/app/projects/project-list.component.ts
+++ src/app/projects/project-list.component.ts
@@ -5,7 +5,8 @@ import { Project } from '../shared/Project';
 
 @Component({
     selector: 'ngptt-project-list',
-    templateUrl: './project-list.component.html'
+    templateUrl: './project-list.component.html',
+    styleUrls: ['./project-list.component.css']
 })
 export class ProjectListComponent {
     projects: Project[] = [

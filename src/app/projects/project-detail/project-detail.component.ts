1f28bfc5419d6a37a3e4644ce4cd20a7651fd1c5
Francesco Sciuti <francescosciuti@Francescos-MBP.homenet.telecomitalia.it>
2019-03-08 09:37:25 +0100

    Update ProjectDetail Component with single responsability

--- src/app/projects/project-detail/project-detail.component.ts
+++ src/app/projects/project-detail/project-detail.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Project } from '../../shared/Project';
-import { ActivatedRoute, Route, ParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { ProjectService } from '../../shared/services/project.service';
-import { Subscription, merge } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
-
-import { DatePipe } from '@angular/common';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'ngptt-project-detail',
   templateUrl: './project-detail.component.html',
@@ -20,80 +16,13 @@ import { DatePipe } from '@angular/common';
   }
   `]
 })
-export class ProjectDetailComponent implements OnInit, OnDestroy {
-  project: Project;
-  projectSubscription: Subscription;
-  editProjectForm: FormGroup;
-  editMode = false;
-
+export class ProjectDetailComponent implements OnInit {
+  project$: Observable<Project>;
+  
   constructor(private route: ActivatedRoute, private projectService: ProjectService) { }
 
   ngOnInit() {
-    this.projectSubscription = merge(
-      this.route.paramMap.pipe(
-        switchMap((params: ParamMap) => this.projectService.get(params.get('code'))),
-      ),
-      this.projectService.project$
-    ).subscribe((project) => {
-      this.project = project;
-      this.initForm();
-    })
-  }
-
-  initForm() {
-    const datePipe = new DatePipe('en-US');
-
-    const projectTasks = new FormArray([]);
-    if (this.project.tasks) {
-      for (const task of this.project.tasks) {
-        projectTasks.push(
-          new FormGroup({
-            'name': new FormControl(task.name, Validators.required),
-            'start': new FormControl(datePipe.transform(task.start, 'yyyy-MM-dd'), Validators.required),
-            'duration': new FormControl(task.duration, Validators.required),
-            'isBillable': new FormControl(task.isBillable)
-          })
-        );
-      }
-    }
-
-    this.editProjectForm = new FormGroup({
-      'code': new FormControl(this.project.code, Validators.required),
-      'name': new FormControl(this.project.name, Validators.required),
-      'description': new FormControl(this.project.description),
-      'priority': new FormControl(this.project.priority, Validators.required),
-      'start': new FormControl(datePipe.transform(this.project.start, 'yyyy-MM-dd'), Validators.required),
-      'end': new FormControl(datePipe.transform(this.project.end, 'yyyy-MM-dd')),
-      'tasks': projectTasks
-    });
-  }
-
-  onAddTask() {
-    const control = new FormControl(null, Validators.required);
-    (<FormArray>this.editProjectForm.get('tasks')).push(
-      new FormGroup({
-        'name': new FormControl(null, Validators.required),
-        'start': new FormControl(null, Validators.required),
-        'duration': new FormControl(null),
-        'isBillable': new FormControl(false)
-      })
-    );
-  }
-
-  onDeleteTask(index: number) {
-    (<FormArray>this.editProjectForm.get('tasks')).removeAt(index);
-  }
-
-  toggleEditMode() {
-    this.editMode = !this.editMode;
-  }
-
-  submitProjectForm() {
-    this.projectService.update(this.editProjectForm.value);
-    this.toggleEditMode();
-  }
-
-  ngOnDestroy() {
-    this.projectSubscription.unsubscribe();
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.project$ = this.projectService.get(id);
   }
 }
